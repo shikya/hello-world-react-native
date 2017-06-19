7c008bde8e28e46a99596ceae1a94009e5b039e9
Shrikant Sonone <shrikant.sonone@coda.global>
2017-06-19 19:32:28 +0530

    Added a simple Paper element.

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Paper } from './src/Paper';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={styles.mainView}>
+        <Paper />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  mainView: {
+    marginTop: 20,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
